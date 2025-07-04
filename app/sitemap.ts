179ee220b5abd7adf69a6e83157d0667593ba8e1
AriefDaffa <ariefkeren76@yahoo.com>
2025-07-04 11:29:04 +0700

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://ariefdaffa.dev',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://ariefdaffa.dev/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://ariefdaffa.dev/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://ariefdaffa.dev/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
+}
